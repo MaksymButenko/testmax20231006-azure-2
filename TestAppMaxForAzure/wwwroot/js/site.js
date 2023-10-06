1356783e30c6d70c50cf7a29fc81df259c3f14ad
Maksym Butenko <maksym.butenko@gmail.com>
2023-10-06 17:03:05 +0300

    add linter

--- TestAppMaxForAzure/wwwroot/js/site.js
+++ TestAppMaxForAzure/wwwroot/js/site.js
@@ -31,4 +31,4 @@ function getNextSaturday(date = new Date()) {
 window.addEventListener("load", function () {
     var days = (getNextSaturday().getTime() - (new Date()).getTime()) / (1000 * 60 * 60 * 24);
     document.querySelector("#daysLeftContainer").innerHTML = "До наступної субботи залишилося " + days + getDaysCaption(days);
-})
+});
